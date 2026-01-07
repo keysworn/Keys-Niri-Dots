4764e0d5474ac80318b023ae49061531bf17d00c
keysworn <shxtings@gmail.com>
2026-01-07 13:51:18 -0500

    Initial dotfiles

---
+++ config/fish/config.fish
@@ -0,0 +1,16 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+fish_add_path /home/key/.spicetify
+
+# pnpm
+set -gx PNPM_HOME "/home/key/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
